cf8fe80ee5d874cc70bfb4067ab4becef2bc8262
Googler <intellij-github@google.com>
2020-04-16 06:53:18 -0700

    Add a package statement to Repackager.java.
    PiperOrigin-RevId: 306836953

--- build_defs/Repackager.java
+++ build_defs/Repackager.java
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.google.idea.repackager;
+
 import static com.google.common.base.Preconditions.checkArgument;
 
 import com.google.common.base.Splitter;
